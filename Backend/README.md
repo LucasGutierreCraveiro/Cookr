8200272eb1950cbcefbbc4ae33649bbc5cd28d70
LucasGutierreCraveiro <lucasgutierrecraveiro@outlook.com>
2021-03-17 17:13:13 -0300

    Update na arquitetura de pasta

--- Backend/README.md
+++
@@ -1,2 +0,0 @@
-# Cookr
-Cookr is a cooking App
--- cookr/src/components/Form/Button/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-// import { Container } from './styles';
-
-const Button: any = () => <div />;
-
-export default Button;
