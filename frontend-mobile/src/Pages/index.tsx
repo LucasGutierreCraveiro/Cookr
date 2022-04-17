7682738b4b71e6013f3e503157e13357c35cffc2
Lucas Craveiro <lucasgutierrecraveiro@outlook.com>
2022-04-17 16:40:56 -0300

    fix files

--- frontend-mobile/src/Pages/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as SignIn } from './SignIn';
-export { default as Pantry } from './Pantry';
-export { default as Home } from './Home';
-export { default as Cookrbook } from './Cookrbook';
